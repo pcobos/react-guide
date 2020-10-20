a205e378b54ee10d23428a449d3986e3483c2855
Pedro Cobos <pedro.cobos@live.com>
2020-10-19 22:47:25 -0500

    created first react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// This is the single component being rendered on index.js. All of the other components we will use go inside this one.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Obaaaa</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
