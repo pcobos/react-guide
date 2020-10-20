e65e6d007103307596eaa4805d96fc62b5a1c453
Pedro Cobos <pedro.cobos@live.com>
2020-10-19 23:12:29 -0500

    added comments to App.js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+// Notice how the APP class inherits from the COMPONENT Class, imported from the react library
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 // This is the single component being rendered on index.js. All of the other components we will use go inside this one.
 class App extends Component {
+  // This class (APP) has one method, the render one. Super Important, react will call this method to render something to the screen
   render() {
+    // Important to remember that something always (HTML) needs to be rendered to the screen.
     return (
+      // THis looks by HTML but it is actually JSX
       <div className="App">
         <h1>Obaaaa</h1>
       </div>
@@ -13,4 +16,5 @@ class App extends Component {
   }
 }
 
+// This class is then exported to the index.js file, which renders it
 export default App;
