f4a3530c95714f83b6aeb63301a4204b59233780
Pedro Cobos <pedro.cobos@live.com>
2020-11-04 20:53:37 -0600

    implemented useState method to update current state

--- src/App.js
+++ src/App.js
@@ -15,6 +15,18 @@ class App extends Component {
       { name: "Gaby", age: 36 }
     ]
   }
+
+  // By convention, is better to use the word Handler when declaring event handler functions (this functions are not tipically called but rather assigned)
+  switchNameHandler = () => {
+    // Following method (setState) is a special one provided by react to update the "state" object
+    this.setState({
+      persons: [
+        { name: "Pedro the immortal", age: 1000 },
+        { name: "Thalia", age: 32 },
+        { name: "Gaby", age: 36 }
+      ]
+    })
+  }
   // This class (APP) has one method, the render one. Super Important, react will call this method to render something to the screen
   render() {
     // Important to remember that something always (HTML) needs to be rendered to the screen.
@@ -22,6 +34,9 @@ class App extends Component {
       // THis looks by HTML but it is actually JSX
       <div className="App">
         <h1>Obaaaa</h1>
+        <h2>This is really working</h2>
+        {/* On the following component we added an event listener (onClick). Notice that the syntax for JSX is different to normal JS (onclick). Once we declared the event, we assign as a Value the code that we want to be executed (eventName={codetobeexecuted}) */}
+        <button onClick={this.switchNameHandler}>Switch name</button>
         {/*First three components are built using state. When state changes, it will prompt the DOM to rerender the component */}
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
