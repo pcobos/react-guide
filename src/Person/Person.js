1af4b42385bc0cd81fe7a34a1fdf37d487a12941
Pedro Cobos <pedro.cobos@live.com>
2020-12-02 21:58:30 -0600

    added style file

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
+    // Remember to assign the className to the component
+    <div className="Person">
       <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
       {/* children property is a reserved word for displaying the stuff inside the element tags when calling our component on the App class */}
       <p>{props.children}</p>
