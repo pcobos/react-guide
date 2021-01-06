efdffe523095f4fe7020a2a085a4b4c2be06a59d
Pedro Cobos <pedro.cobos@live.com>
2021-01-06 17:41:17 -0600

    feat: add style to UserInput component

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 
 const UserInput = (props) => {
+
+  const styles = {
+      width: '100%',
+      padding: "12px 20px",
+      margin: "8px 0px",
+  };
+
   return (
+    
+
     <div>
       <h1>This is my User Input component</h1>
-      <input type="text" onChange={props.change} value={props.username}/>
+      <input style={styles} type="text" onChange={props.change} value={props.username} />
     </div>
   )
 };
